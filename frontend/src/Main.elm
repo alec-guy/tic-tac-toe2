cc73918b056db3352cc1ef5f59e725b2f97d3986
alec <alecrodriguez1247@gmail.com>
2024-11-28 20:21:35 -0600

    commit

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -24,7 +24,7 @@ main =
 
 type alias Model = 
                  {myBoard : Board
-                 ,playingAs : MaybeShape
+                 ,playingAs : Maybe Shape
                  }
 
 
@@ -47,54 +47,70 @@ initialBoard =
 updateBoard : Shape -> (Int,Int) -> Board -> Board 
 updateBoard s (r,c) board = 
      case r of 
-      1 ->  Array.set c s (board.rowOne)
-      2 -> Array.set c s (board.rowTwo)
-      3 -> Array.set c s (board.rowThree)
+      1 ->  {board | rowOne = Array.set c (showShape s) (board.rowOne)}
+      2 -> {board | rowTwo = Array.set c (showShape s) (board.rowTwo)}
+      3 -> {board | rowThree = Array.set c (showShape s) (board.rowThree)}
       _ -> board
 
-getCell : (Int, Int) -> String 
+showShape : Shape -> String 
+showShape s = case s of 
+               Circle -> "Circle"
+               X      -> "X"
+
+getCell : (Int, Int) -> Board -> String 
 getCell (r,c) board = 
    case r of 
-    1 -> case Array.get c sof 
+    1 -> case Array.get c board.rowOne of 
            Nothing -> ""
            (Just v) -> v
-    2 -> case Array.get c sof 
+    2 -> case Array.get c board.rowTwo of 
            Nothing -> ""
            (Just v) -> v
-    3 -> case Array.get c sof 
+    3 -> case Array.get c board.rowThree of 
            Nothing -> ""
            (Just v) -> v
-    _ -> board
+    _ -> ""
 -- UPDATE
 
-data Shape = Circle 
-           | X 
-
-type Msg = ClickedMe (Int Int)
+type Shape = Circle | X 
+notShape : Shape -> Shape 
+notShape s = case s of 
+              Circle -> X 
+              X      -> Circle 
+type Msg = ClickedMe (Int,Int)
          | Chose Shape
          | NPCChoice (Int,Int)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = (model,Cmd.none)
+update msg model = 
   case msg of 
-   (Chose shape) -> ({model | playingAs = Just shape} Cmd.none)
+   (Chose shape) -> ({model | playingAs = Just shape}, Cmd.none)
    (ClickedMe t) -> case model.playingAs of 
                      Nothing -> (model,Cmd.none)
                      (Just s) -> ({model | myBoard = updateBoard s t model.myBoard}
-                                 , generate NPCChoice uniformRowPicker)
+                                 , generate NPCChoice (uniformRowPicker model.myBoard)
+                                 )
    (NPCChoice t) -> case model.playingAs of 
                      Nothing  -> (model,Cmd.none)
-                     (Just s) -> ({model | myBoard = updateBoard (notShape s) t model.myBoard} Cmd.none)
+                     (Just s) -> ({model | myBoard = updateBoard (notShape s) t model.myBoard}, Cmd.none)
 
-
-uniformRowPicker : Model -> Generator (Int,Int)
-uniformRowPicker = 
-      uniform (1,1) ([(1,1),(1,2),(1,3)
-                     ,(2,1),(2,2),(2,3)
-                     ,(3,1),(3,2),(3,3)
-                     ]
-                    )
+getAvailableSpots : Board -> List (Int,Int)
+getAvailableSpots board = 
+        let rowOneSpots  = loopingFunction 1 0 board.rowOne 
+            rowTwoSpots  = loopingFunction 2 0 board.rowTwo 
+            rowThreeSpots = loopingFunction 3 0 board.rowThree 
+        in rowOneSpots ++ rowTwoSpots ++ rowThreeSpots
+loopingFunction : Int -> Int -> Array String -> List (Int,Int)
+loopingFunction i acc array = 
+       case Array.toList array of 
+        [] -> [] 
+        (s :: ss) -> case s of 
+                      "" -> (i, 1 + acc) :: (loopingFunction i (acc + 1) (Array.fromList ss))
+                      _  -> loopingFunction i (acc + 1) (Array.fromList ss)
+uniformRowPicker : Board -> Generator (Int,Int)
+uniformRowPicker board = 
+      uniform (1,1) (getAvailableSpots board)
 
 -- SUBSCRIPTIONS
 
@@ -110,11 +126,12 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [ text "Tic-Tac-Two version2 by Alec Rodriguez"]
-    , chooseShape 
-    , viewBoard model
-    ]
+  case model.playingAs of 
+   Nothing -> chooseShape 
+   (Just _) -> div []
+               [ h2 [] [ text "Tic-Tac-Two version2 by Alec Rodriguez"]
+               , viewBoard model
+               ]
 
 chooseShape : Html Msg 
 chooseShape = div 
@@ -130,22 +147,38 @@ viewBoard model =
    Nothing       -> text "Choose your shape before you can play."
    _             -> div 
                     boardAttributes 
-                    [div[style "text-align" "center", onClick <| ClickedMe (1,1)][text <| getCell (1,1) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (1,2)][text <| getCell (1,2) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (1,3)][text <| getCell (1,3) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (2,1)][text <| getCell (2,1) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (2,2)][text <| getCell (2,2) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (2,3)][text <| getCell (2,3) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (3,1)][text <| getCell (3,1) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (3,2)][text <| getCell (3,2) model.myBoard]
-                    ,div[style "text-align" "center", onClick <| ClickedMe (3,3)][text <| getCell (3,3) model.myBoard]
+                    [div(cellAttributes ++ [onClick <| ClickedMe (1,1)])[text <| getCell (1,1) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (1,2)])[text <| getCell (1,2) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (1,3)])[text <| getCell (1,3) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (2,1)])[text <| getCell (2,1) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (2,2)])[text <| getCell (2,2) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (2,3)])[text <| getCell (2,3) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (3,1)])[text <| getCell (3,1) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (3,2)]) [text <| getCell (3,2) model.myBoard]
+                    ,div(cellAttributes ++ [onClick <| ClickedMe (3,3)] )[text <| getCell (3,3) model.myBoard]
                     ]
 
 
  
-boardAttributes : List Attributes
+ -----------------
+ --- AI MADE WHAT IS BELOW ME 
+ 
+boardAttributes : List (Html.Attribute Msg )
 boardAttributes =         
-        [style "display", "grid"
+        [style "display" "grid"
         ,style "grid-template-columns" "repeat(3, 1fr)"
         ,style "gap" "10px"
+        ,style "width" "300px"
+        ,style "margin" "0 auto"
+        ,style "border" "2px solid black"
+        ,style "padding" "10px"
         ] 
+
+cellAttributes : List (Html.Attribute Msg)
+cellAttributes = 
+        [style "border" "1px solid black"
+        ,style "text-align" "center"
+        ,style "padding" "20px"
+        ,style "background-color" "#f0f0f0"
+        ,style "cursor" "pointer"
+        ]
